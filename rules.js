5d7540b46a91febabf3c4570b550aa062f5015a7
Hlieb P <pantushenko11@gmail.com>
2023-08-14 12:06:56 +0300

    design fixed and rules bag fixed commit

---
+++ rules.js
@@ -0,0 +1,14 @@
+function readMore(){
+    var dots = document.getElementById("dots");
+    var more = document.getElementById("more");
+    var btn = document.getElementById("btn");
+    if(dots.style.display === "none"){
+        dots.style.display = "inline";
+        btn.innerHTML = "Правила";
+        more.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Скрыть";
+        more.style.display = "flex";
+    }
+}
